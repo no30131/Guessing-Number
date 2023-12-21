9b70a87e2deadf1da45148b3bd559478da35b414
Melody Zhao <no30131@gmail.com>
2023-12-22 00:52:12 +0800

    Full Stack Assignment(not yet completed)

--- advanced-assignment-node/index.js
+++ advanced-assignment-node/index.js
@@ -1,9 +1,8 @@
-// index.js
-
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 const app = express()
-const PORT = 3000
+const PORT = 3001
 const mongoose = require('mongoose')
 
 require('dotenv').config()
@@ -26,14 +25,77 @@ mongoose
 
 app.use(express.json())
 
-app.get("/test", (req, res) => {
-	res.send({msg: "vegan world!"})
-	//res.status(404).json({ error: 'Page111 did not exist' })
-})
+// app.get("/test", (req, res) => {
+// 	res.send({msg: "vegan world!"})
+// 	//res.status(404).json({ error: 'Page111 did not exist' })
+// })
 
-app.get("/answer", (req, res) => {
-	res.send({answer: Math.floor(Math.random() * 100) + 1})
-})
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	optionsSuccessStatus: 200
+}
+
+app.use(cors()); 
+		
+
+// 定義 Schema
+const gameSchema = new mongoose.Schema({
+	createdAt: {
+	  type: Date,
+	  default: Date.now
+	},
+	numberOfGuesses: Number,
+	answer: Number,
+	isSuccess: Boolean
+  });
+  
+  // 創建模型
+  const Game = mongoose.model('Game', gameSchema);
+  
+  app.get("/answer", async (req, res) => {
+	try {
+	  // 獲取隨機答案
+	  const randomAnswer = Math.floor(Math.random() * 100) + 1;
+  
+	  // 創建新的遊戲文檔
+	//   const newGame = new Game({
+	// 	numberOfGuesses: 0,
+	// 	answer: randomAnswer,
+	// 	isSuccess: false
+	//   });
+  
+	  // 將新遊戲保存到數據庫
+	//   await newGame.save();
+  
+	  res.send({ answer: randomAnswer });
+	} catch (error) {
+	  console.error('Error:', error);
+	  res.status(500).send('Error generating answer');
+	}
+  });
+
+  app.post("/record", async (req, res) => {
+	// 獲取前端發送的資料
+	const { isSuccess, targetNum, guessNum } = req.body; 
+
+	// 創建一個新的遊戲文檔
+	const newGame = new Game({
+	  numberOfGuesses: guessNum,
+	  answer: targetNum,
+	  isSuccess: isSuccess
+	});
+  
+	try {
+	  // 將新遊戲保存到數據庫
+	  const savedGame = await newGame.save();
+	  console.log('Saved game to database:', savedGame);
+	  res.status(200).send({ message: 'Game data saved to database' });
+  	} catch (error) {
+	  console.error('Error saving game data:', error);
+	  res.status(500).send({ error: 'Error saving game data' });
+	}
+  });
 
 app.use((err, req, res, next) => {
 	const status = err.status || 500
@@ -41,7 +103,7 @@ app.use((err, req, res, next) => {
 		console.log('The server errored when processing a request')
 		console.log(err)
 	}
-
+	
 	res.status(status)
 	res.send({
 		status: status,
--- advanced-assignment-react/src/App.js
+++ advanced-assignment-react/src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const baseURL = "http://localhost:3001";
 
 function App() {
-  useEffect(() => {
-    const randomNum = () => {
-      const num = Math.floor(Math.random() * 100) + 1;
-      setTargetNum(num);
-    };
-    randomNum();
-  }, []);
-  
+  // useEffect(() => {
+  //   const randomNum = () => {
+  //     const num = Math.floor(Math.random() * 100) + 1;
+  //     setTargetNum(num);
+  //   };
+  //   randomNum();
+  // }, []);
+
+  // useEffect(() => {
+  //   axios.get(`${baseURL}/answer`)
+  //     .then((response) => {
+  //       setTargetNum(response.data.answer);
+  //       // console.log(`取得${targetNum}`);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching data: , error');
+  //     });
+  // },[]);
+    
   // init
-  const [targetNum, setTargetNum] = useState(0);
+  const [targetNum, setTargetNum] = useState(
+    () => {
+    axios.get(`${baseURL}/answer`)
+      .then((response) => {
+        setTargetNum(response.data.answer);
+        console.log(`取得${targetNum}`);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: , error');
+      });
+  },[]);
+  // const [targetNum, setTargetNum] = useState(0);
   const [guessNum, setGuessNum] = useState("");
   const [message, setMessage] = useState("");
   const [firstNum, setFirstNum] = useState(1);
   const [secondNum, setSecondNum] = useState(100);
-
+  const [isSuccess, setIsSuccess] = useState(false);
+  
   console.log("answer: " + targetNum);
 
   // guessing number
@@ -25,8 +51,11 @@ function App() {
     const userGuess = parseInt(guessNum);
     console.log(`userGuess: ${userGuess}`);
 
+    let currentIsSuccess = false;
+
     if (userGuess === targetNum) {
       setMessage(`恭喜猜對！答案是 ${targetNum}`);
+      currentIsSuccess = true;
     } else if (userGuess > targetNum){  
       setMessage("太大了！");
       setSecondNum(userGuess);
@@ -34,7 +63,28 @@ function App() {
       setMessage("太小了！");
       setFirstNum(userGuess);
     }
-  }
+  
+    setIsSuccess(currentIsSuccess);
+
+  // 用 useEffect 來監聽猜對猜錯狀態的改變
+  // useEffect(() => {
+    console.log("isSuccess:", isSuccess);
+  // });
+
+  const dataToSend = {
+    isSuccess: isSuccess,
+    targetNum: targetNum,
+    guessNum: guessNum
+  };
+
+  axios.post(`${baseURL}/record`, dataToSend)
+    .then((response) => {
+      console.log(`返回${response.data}`);
+    })
+    .catch((error) => {
+      console.error('Error sending data to backend :, error');
+    });
+}
 
   return (
     <div className="App">
